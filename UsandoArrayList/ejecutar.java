1dba90b5b053502221e1b705cb9b9b6a8c8c3c1a
rgfigueroa <roberthgfd@gmail.com>
2021-12-08 13:38:10 -0500

    Add files via upload

--- UsandoArrayList/ejecutar.java
+++ UsandoArrayList/ejecutar.java
@@ -12,6 +12,7 @@ public class ejecutar {
         persona personaUno= new persona(20,"Juan Perez");
         persona pablo= new persona(22,"Pablo Pilco");
         persona yovin= new persona(20,"Yovin Urrego");
+        persona Thais = new persona(18,"Thais Cartuche");
 
         ArrayList<persona> listaPersona = new ArrayList<persona>();
         listaPersona.add(personaUno);
@@ -25,11 +26,30 @@ public class ejecutar {
 
         yovin.setNombre("yovin Steven");
         
-        for (persona lp : listaPersona) {
+        for (persona lp: listaPersona) {
             System.out.println(lp.getNombre()+"--"+lp.getEdad());
         }
+        
         //Notacion lambda.
+        listaPersona.forEach(p -> System.out.println("Dato:"+p.getEdad()));
 
+        //Ejemplo de uso de auto y persona
+        auto hyundai = new auto("LBA0727",5000,yovin);
+        auto autoKia= new auto("LBB0237",6000,pablo);
+        auto autoFord= new auto("XYZ0123",20000,Thais);
+
+        ArrayList <auto> patioautos = new ArrayList<auto>();
+        patioautos.add(hyundai);
+        patioautos.add(autoKia);
+        patioautos.add(autoFord);
+
+
+        for(auto a: patioautos){
+            System.out.println(a.getPlaca()+"--" + a.getPropietario().getNombre()+" su costo es: "+a.getPrecio());
+        }
+        
+        //Notación Lambda.
+        patioautos.forEach(a -> System.out.println(a.getPrecio()));
+       
     }
-    
 }
